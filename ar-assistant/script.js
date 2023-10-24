142be974ef1cf9dbf4e5bd19110679c8fd0999fc
uttesh <uttesh@gmail.com>
2023-10-24 13:12:59 +0530

    Added help text

--- ar-assistant/script.js
+++ ar-assistant/script.js
@@ -7,6 +7,18 @@ window.onload = () => {
 };
 
 function staticLoadPlaces() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      document.getElementById(
+        "help-text"
+      ).innerText = `Latitude: ${latitude}, Longitude: ${longitude}`;
+    });
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
   return [
     {
       name: "Pokèmon",
