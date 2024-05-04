4e364f9101100a5a97d90f09bf06a9a26e8af52e
uttesh <uttesh@gmail.com>
2024-05-04 15:49:11 +0530

    event added

---
+++ ar-assistant/location/cube-component.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent("cube", {
+  schema: {},
+  init: function () {
+    console.log("init....");
+    var lastIndex = -1;
+    var COLORS = ["red", "green", "blue"];
+    this.el.addEventListener("click", function (evt) {
+      lastIndex = (lastIndex + 1) % COLORS.length;
+      this.setAttribute("material", "color", COLORS[lastIndex]);
+      console.log("I was clicked at: ", evt.detail.intersection.point);
+    });
+  },
+  update: function () {
+    console.log("update....");
+  },
+  //   tick: function () {
+  //     console.log("ticket....");
+  //   },
+  remove: function () {
+    console.log("remove....");
+  },
+  pause: function () {
+    console.log("pause....");
+  },
+  play: function () {
+    console.log("play....");
+  }
+});
