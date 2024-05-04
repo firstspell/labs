4d9eec07f217cc42d1b568625f103d599e56ee0c
uttesh <uttesh@gmail.com>
2024-05-04 20:02:48 +0530

    updated the click event

--- ar-assistant/location/cube-component.js
+++ ar-assistant/location/cube-component.js
@@ -4,7 +4,7 @@ AFRAME.registerComponent("cube", {
     console.log("init....");
     var lastIndex = -1;
     var COLORS = ["red", "green", "blue"];
-    this.el.addEventListener("touchstart", function (evt) {
+    this.el.addEventListener("click", function (evt) {
       lastIndex = (lastIndex + 1) % COLORS.length;
       this.setAttribute("material", "color", COLORS[lastIndex]);
       console.log("I was clicked at: ", evt.detail.intersection.point);
