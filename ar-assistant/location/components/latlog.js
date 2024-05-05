b4fa5de7473e0f52bd48eab48e6a7fb55da204ad
uttesh <uttesh@gmail.com>
2024-05-05 19:27:46 +0530

    updated

---
+++ ar-assistant/location/components/latlog.js
@@ -0,0 +1,10 @@
+AFRAME.registerComponent("latlog", {
+  tick: function () {
+    // `this.el` is the element.
+    // `object3D` is the three.js object.
+    // `rotation` is a three.js Euler using radians. `quaternion` also available.
+    // console.log(this.el.object3D.rotation);
+    // `position` is a three.js Vector3.
+    // console.log(this.el.object3D.position);
+  }
+});
